95280313e6b9c988b0feaaed443472b8ed4e17c1
BaoHoang0311 <tan.thach36@gmail.com>
2022-11-30 18:29:48 +0700

    product

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -8,9 +12,9 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+         
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
---
+++ API/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace API.Model
+{
+    public class Product
+    {
+        public string productId { get; set; }
+        public string productName { get; set; }
+        public string productOwner { get; set; }
+        public double Amount { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
